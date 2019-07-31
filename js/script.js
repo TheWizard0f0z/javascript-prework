00502d85a6e9b694531331fd3a5d683ed8146bb6
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-07-31 17:15:48 +0200

    Add js code

--- js/script.js
+++ js/script.js
@@ -29,3 +29,21 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+} else if( computerMove == 'kamień' || 'nożyce' || 'papier' && playerMove == 'nieznany ruch'){
+    printMessage('Oszukujesz!');
+} else{
+    printMessage('Ty przegrywasz!');
+}
